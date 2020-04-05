8d24163fac85b87e549cbb87952766b6819e992e
comp <comp@DESKTOP-CSB55GA>
2020-04-05 20:15:48 +0530

    First commit

---
+++ mymavenproject/src/test/java/pm/orglearnigmaven/mymavenproject/HellomavenTest.java
@@ -0,0 +1,13 @@
+package pm.orglearnigmaven.mymavenproject;
+
+import org.testng.annotations.Test;
+
+public class HellomavenTest {
+	@Test
+	public void demo()
+	{
+		System.out.println("This is maven demo");
+	}
+	
+
+}
